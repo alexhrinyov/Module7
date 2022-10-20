c99d2c06ee1f6870e2c3a7bbad5eb8fd48247c28
alexhrinyov <hrinyou29@gmail.com>
2022-10-20 17:38:41 +0300

    Unit 6 added

---
+++ Unit6/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit6
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            
+        }
+        
+    }
+    //Создайте класс-обобщение Car для автомобиля. Универсальным параметром будет
+    //тип двигателя в автомобиле (электрический и бензиновый). Для типов двигателей также создайте классы —
+    //ElectricEngine и GasEngine.
+    //В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.
+    class Car<TEngine> where TEngine: Engine       
+    {
+        public TEngine EngineType;
+        
+        public virtual void ChangePart<TPart>(TPart newPart) where TPart: PartType 
+        {
+
+        }
+    }
+
+    class ElectricEngine: Engine
+    {
+
+    }
+    class GasEngine : Engine
+    {
+
+    }
+    class Battery: PartType
+    {
+
+    }
+    class Differentioal : PartType
+    {
+
+    }
+    class Wheel : PartType
+    {
+
+    }
+    class Engine
+    {
+
+    }
+    class PartType
+    {
+
+    }
+
+    //7.6.6
+    class Record<T1, T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+    }
+
+
+
+    
+
+}
