e6469d56ee811de225eb28cefc93cceb7bc2724c
alexhrinyov <hrinyou29@gmail.com>
2022-10-21 20:45:44 +0300

    lastcommit

--- FinalTask7/Program.cs
+++ FinalTask7/Program.cs
@@ -10,9 +10,63 @@ namespace FinalTask7
 {
     internal class Program
     {
+        public static HeartRateMonitor[] heartRateMonitors = new HeartRateMonitor[3];
+        public static HeartRateSensor[] heartRateSensors = new HeartRateSensor[2];
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Приветствуем вас в нашем интернет-магазине! Из-за сложившейся" +
+                "геополитеческой обстановки объявляем распродажу!");
+            Console.WriteLine("В настоящее время в нашем магазине представлены следующие модели устройств:");
+            Console.WriteLine("Пульсометры:");
+            FillProductRange();
+            for (int i = 0; i < heartRateMonitors.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {heartRateMonitors[i].Name}");
+            }
+            Console.WriteLine("Датчики измерения ЧСС:");
+            for (int i = 0; i < heartRateSensors.Length; i++)
+            {
+                Console.WriteLine($"{i + 4}. {heartRateSensors[i].Type},{heartRateSensors[i].IP}");
+            }
+            Console.WriteLine("Для совершения заказа нажмите соответствующую цифру: ");
+            ConsoleKeyInfo Choose = Console.ReadKey();
+            for (int i = 0; i < heartRateMonitors.Length; i++)
+            {
+                if (Choose.Key == (ConsoleKey)(i+49))
+                {
+                    Console.WriteLine($"Вы выбрали {heartRateMonitors[i].Name}");
+
+                }
+            }
+            for (int i = 0; i < heartRateSensors.Length; i++)
+            {
+                if (Choose.Key == (ConsoleKey)(i + 52))
+                {
+                    Console.WriteLine($"Вы выбрали датчик {heartRateSensors[i].Type}");
+
+                }
+            }
+            /// и т.д.
+
+        }
+        static void FillProductRange()
+        {
+            HeartRateMonitor hm1 = new HeartRateMonitor("Polar H178", "Цифровой", true, true, true, "Li-ion", 150,1.5);
+            hm1.IP = "IP68";
+            HeartRateMonitor hm2 = new HeartRateMonitor("Polar H125", "Аналоговый", true, true, false, "CR2032");
+            hm2.IP = "IP68";
+            HeartRateMonitor hm3 = new HeartRateMonitor("Sigma 15.11", "Аналоговый", true, true, false, "CR2032");
+            hm3.IP = "IP68";
+            HeartRateSensor sens1 = new HeartRateSensor("Нагрудный", true, "Li-ion", 100, 1);
+            sens1.IP = "IP68";
+            HeartRateSensor sens2 = new HeartRateSensor("Оптический", true, "Ni-Ca",150, 5.5);
+            sens2.IP = "IP68";
+            heartRateMonitors[0] = hm1;
+            heartRateMonitors[1] = hm2;
+            heartRateMonitors[2] = hm3;
+            heartRateSensors[0] = sens1;
+            heartRateSensors[1]= sens2;
+
         }
     }
     static class TimeOfDelivery
@@ -48,12 +102,12 @@ namespace FinalTask7
     {
         private string address;
         private string note;
-        public string Address
+        protected string Address
         {
             get { return address; }
             set { address = value; }
         }
-        public string Note
+        protected string Note
         {
             get { return note; }
             set { note = value; }
@@ -86,7 +140,10 @@ namespace FinalTask7
             Console.WriteLine(Delivery.City);
             
         }
-        
+        public void GetProduct()
+        {
+
+        }
                
     }
 
@@ -178,14 +235,14 @@ namespace FinalTask7
         //с аккумулятором
         public HeartRateSensor(string type, bool IsRechargable, string TypeOfBattary,int Capacity,double TimeOfCharge)
         {
-
+            
             Type = type;
             battery = new Battery(IsRechargable, TypeOfBattary, Capacity,TimeOfCharge);
         }
     }
     class HeartRateMonitor:Device
     {
-        
+        public string Name;
         public string TypeOfSignal;
         public bool HeartRateMax;
         public bool Time;
@@ -201,18 +258,18 @@ namespace FinalTask7
             Console.WriteLine($"Степень защиты: {IP}");
         }
         // на батарейках
-        public HeartRateMonitor(string TypeOfSignal,bool HeartRateMax,bool Time, bool IsRechargable, string TypeOfBattary)
+        public HeartRateMonitor(string Name,string TypeOfSignal,bool HeartRateMax,bool Time, bool IsRechargable, string TypeOfBattary)
         {
-
+            this.Name = Name;
             this.TypeOfSignal = TypeOfSignal;
             this.HeartRateMax = HeartRateMax;
             this.Time = Time;
             battery = new Battery(IsRechargable, TypeOfBattary);
         }
         //с аккумулятором
-        public HeartRateMonitor(string TypeOfSignal, bool HeartRateMax, bool Time, bool IsRechargable, string TypeOfBattary, int Capacity, double TimeOfCharge)
+        public HeartRateMonitor(string Name,string TypeOfSignal, bool HeartRateMax, bool Time, bool IsRechargable, string TypeOfBattary, int Capacity, double TimeOfCharge)
         {
-
+            this.Name = Name;
             this.TypeOfSignal = TypeOfSignal;
             this.HeartRateMax = HeartRateMax;
             this.Time = Time;
